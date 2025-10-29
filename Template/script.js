d7ca652b0f9f0c2cd0fdaa62bf93bd2b2a175e12
HtetZawOo <htetzawoo1995@gmail.com>
2025-10-29 23:51:22 +0100

    added more possible functions

--- Template/script.js
+++ Template/script.js
@@ -8,7 +8,7 @@ import * as Util from "./util.js";
 //Global Constants
 const initialSize = 50;
 const targetSize = 250;
-
+const balloonGap = 200;
 //Global Variables
 let players = []
 let nPlayers = 0; //Number of Players
@@ -20,8 +20,32 @@ let assignedKeys = [
   ["c","v"], //Player 3 Keys
   ["n","m"]  //Player 4 Keys
 ];
-
 //Settings variables should contain all of the "fixed" parts of your programs
+
+//Functions
+
+//Start Game function
+function startGame(){
+
+}
+
+//Game Over function
+function gameOver(){
+
+}
+
+//Game Restart function
+function restartGame(){
+
+}
+
+//Create Players and Balloons
+function createPlayers(){
+players = [];
+
+for (let i=)
+}
+
 
 //Code that runs over and over again
 function loop() {
@@ -29,11 +53,11 @@ function loop() {
   window.requestAnimationFrame(loop);
 }
 
-//Setup is run once, at the start of the program. It sets everything up for us!
-function setup() {
+//Starting Screen of the Game
+function startScreen() {
 //Put your event listener code here
 
   window.requestAnimationFrame(loop);
 }
 
-setup(); // Always remember to call setup()!
+startScreen(); // Always remember to call setup()!
